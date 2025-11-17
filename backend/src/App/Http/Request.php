f3128bc5210aa4d1131463bbe2bd6ef155aa55bc
João Ramajo <joaoramajo744@gmail.com>
2025-11-17 20:29:30 -0300

    feat(request): adiciona classe request para abstração das requisições do cliente

---
+++ backend/src/App/Http/Request.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http;
+
+class AppRequest
+{
+    private array $query;
+    private array $body;
+
+    public function __construct()
+    {
+        $this->query = $_GET ?? [];
+        $this->body = json_decode(file_get_contents('php://input'), true) ?? [];
+    }
+
+    public function input(string $key, $default = null)
+    {
+        return $this->body[$key] ?? $this->query[$key] ?? $default;
+    }
+}
