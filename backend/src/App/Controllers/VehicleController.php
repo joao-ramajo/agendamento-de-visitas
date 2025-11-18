d804f1f65af07fd30ce06e72f09df83ade68b7d3
João Ramajo <joaoramajo744@gmail.com>
2025-11-17 21:08:44 -0300

    feat(vehicles): adiciona rota de listagem com dados mockados para análise do domínio

--- backend/src/App/Controllers/VehicleController.php
+++ backend/src/App/Controllers/VehicleController.php
@@ -1,15 +1,29 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Http\Request;
 use App\Http\Response;
 
 class VehicleController
 {
-    public function index()
+    public function index(Request $request)
     {
-        return Response::json(['message' => 'Listando carros']);
+        $payload = [
+            'message' => 'Listagem realizada com sucesso',
+            'vehicles' => [
+                [
+                    'id' => 1,
+                    'image_url' => 'http=> //image.png',
+                    'brand' => 'Volkswagen',
+                    'model' => 'Gol',
+                    'version' => '1.0',
+                    'price' => 123000,
+                    'location' => 'Mogi das Cruzes'
+                ]
+            ]
+        ];
+
+        return Response::json($payload);
     }
 }
