995195d8cf0b7c878d6b91ab94d01474bb7e2f18
João Ramajo <joaoramajo744@gmail.com>
2025-11-18 23:03:46 -0300

    chore(repositories): ajusta identação e remove dependencias não utilizadas

--- backend/src/Domain/Contracts/Repositories/AppointmentRepositoryInterface.php
+++ backend/src/Domain/Contracts/Repositories/AppointmentRepositoryInterface.php
@@ -1,13 +1,12 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Domain\Contracts\Repositories;
 
 use App\Dtos\StoreAppointmentDto;
-use Domain\Entities\Appointment;
-
 interface AppointmentRepositoryInterface
 {
-    public function create(StoreAppointmentDto $data);
+    /**
+     * @return int
+     */
+    public function create(StoreAppointmentDto $data): int;
 }
--- backend/src/Domain/Contracts/Repositories/VehicleRepositoryInterface.php
+++ backend/src/Domain/Contracts/Repositories/VehicleRepositoryInterface.php
@@ -1,10 +1,9 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Domain\Contracts\Repositories;
 
 use Domain\Entities\Vehicle;
+
 interface VehicleRepositoryInterface
 {
     /**
