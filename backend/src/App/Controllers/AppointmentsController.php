0560ed3b389e1eff053ab48507fd2deaa6d1c384
João Ramajo <joaoramajo744@gmail.com>
2025-11-18 22:54:17 -0300

    refactor(appointmentController): adiciona tratamento generico de erros

--- backend/src/App/Controllers/AppointmentsController.php
+++ backend/src/App/Controllers/AppointmentsController.php
@@ -3,19 +3,21 @@
 namespace App\Controllers;
 
 use App\Dtos\StoreAppointmentDto;
-use App\Http\Requests\StoreAppointmentRequest;
 use App\Http\Request;
 use App\Http\Response;
 use App\Services\AppointmentService;
 use Domain\Exceptions\ModelNotFoundException;
 use Domain\Exceptions\SlotNotAvailableException;
 use Infra\Mappers\AppointmentMapper;
+use Monolog\Logger;
+use Exception;
 use InvalidArgumentException;
 
 class AppointmentsController
 {
     public function __construct(
-        protected readonly AppointmentService $appointmentService
+        protected readonly AppointmentService $appointmentService,
+        protected readonly Logger $log,
     ) {}
 
     public function store(Request $request)
@@ -28,12 +30,16 @@ class AppointmentsController
             return Response::json([
                 'data' => AppointmentMapper::toArray($appointment),
             ], 201);
-        } catch (InvalidArgumentException $e) {
-            return Response::error($e->getMessage(), 422);
-        } catch (SlotNotAvailableException $e) {
+        } catch (InvalidArgumentException|SlotNotAvailableException $e) {
             return Response::error($e->getMessage(), 422);
         } catch (ModelNotFoundException $e) {
             return Response::error($e->getMessage(), 404);
+        } catch (Exception $e) {
+            $this->log->error('Erro ao criar agendamento', [
+                'message' => $e->getMessage(),
+                'exception' => $e
+            ]);
+            return Response::error('erro interno do servidor.', 500);
         }
     }
 }
